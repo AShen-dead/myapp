41a587b5b0e1ad5f34da71bf5bc88ffb52793f52
Lemons <leonenmoonierey1@gmail.com>
2025-09-03 01:23:04 +1200

    Create moonie.blade.php
    add my autobiography

---
+++ resources/views/biography/moonie.blade.php
@@ -0,0 +1,22 @@
+<x-layout title="Jacen Luke A. Cielos Biography">
+    <div class="profile-card">
+        <div class="banner">
+            <div class="avatar" style="background-image: url('{{ asset('pictures/moonie.jpg') }}');"></div>
+        </div>
+        <div class="content">
+            <h2>Moonie Rey L. Leonen</h2>
+            <h4>BSIT STUDENT</h4>
+            <h5>Born September 20, 2004</h5>
+            <hr>
+            <h4>ABOUT ME</h4>
+            <p>Hello! I’m Moonie Rey still learning new things in IT</p>
+            <hr>
+            <h4>HOBBIES</h4>
+            <ul>
+                <li>Watching Anime/movies</li>
+                <li>Gaming</li>
+                <li>Reading Comics/Manga</li>
+            </ul>
+        </div>
+    </div>
+</x-layout>
